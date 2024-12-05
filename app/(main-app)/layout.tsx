3f48086c399d910dd11c142f282dd89315129297
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2024-12-05 19:33:26 +0100

    added protection for main app routes

--- app/(main-app)/layout.tsx
+++ app/(main-app)/layout.tsx
@@ -1,14 +1,26 @@
 import Navigation from "@/components/ui/navigation";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/sign-in");
+  }
+
   return (
     <>
       <Navigation />
-      <div>{children}</div>;
+      <div>{children}</div>
     </>
   );
 }
