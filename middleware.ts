136a6b2d4d527eec85b2b8b068525f4ddc1eb43e
Johnny <104537366+johnnysedh3lllo@users.noreply.github.com>
2025-04-26 01:19:26 +0100

    Updated middleware.ts to corrected pathname for stripe webhook

--- middleware.ts
+++ middleware.ts
@@ -15,6 +15,6 @@ export const config = {
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!api/webhooks/.*|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api/webhook/.*|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
